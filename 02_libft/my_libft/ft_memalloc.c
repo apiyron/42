5fbc0ae7dfa5028f5db4d6c5140a73a2071603bc
Andrey Ryabinskiy <apiyron@users.noreply.github.com>
2019-03-19 22:15:02 +0300

    part2: 5/24

---
+++ 02_libft/my_libft/ft_memalloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memalloc.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrhea-ro <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/19 21:22:36 by mrhea-ro          #+#    #+#             */
+/*   Updated: 2019/03/19 21:57:52 by mrhea-ro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memalloc(size_t size)
+{
+	void *fresh;
+
+	fresh = malloc(size);
+	if (!fresh)
+		return (NULL);
+	ft_bzero(fresh, size);
+	return (fresh);
+}
