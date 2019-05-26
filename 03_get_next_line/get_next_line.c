36c19c37a885b0d68d438c3a743fdf467c9fbc3b
Andrey Ryabinskiy <apiyron@users.noreply.github.com>
2019-05-26 18:14:24 +0300

    compile errors

--- 03_get_next_line/get_next_line.c
+++ 03_get_next_line/get_next_line.c
@@ -6,8 +6,57 @@
 /*   By: mrhea-ro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/26 15:53:16 by mrhea-ro          #+#    #+#             */
-/*   Updated: 2019/05/26 15:55:44 by mrhea-ro         ###   ########.fr       */
+/*   Updated: 2019/05/26 18:07:58 by mrhea-ro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+static char		getN(char **g)
+{
+	size_t	i;
+	char 	*str;
+	char	*tmp;
+	char	*tmp2;
+
+	str = *g;
+	i = 0;
+	while (str[i] != '\n' && str[i] != '\0')
+		i++;
+	tmp = ft_strnew(i);
+	tmp = ft_strncpy(tmp, str, i);
+	tmp2 = ft_strnew(ft_strlen(&(str[i + 1])));
+	tmp2 = ft_strncpy(tmp2, &str[i + 1], ft_strlen(&str[i]));
+	free(*g);
+	*g = tmp2;
+	return (tmp);
+}
+
+int				get_next_line(const int fd, char **line)
+{
+	static char		*memory[12000];
+	char			buff[BUFF_SIZE + 1];
+	int				check;
+
+	if ((read(fd, buff, 0) < 0 || line == NULL))
+		return (-1);
+	while (ft_findchar(memory[fd], '\n') == 0)
+	{
+		check = read(fd, buff, BUFF_SIZE);
+		buff[check] = '\0';
+		if (check <= 0)
+			break;
+		memory[fd] = ft_strjoinfree(memory[fd], buff);
+	}
+	if (ft_strlen(memory[fd]) || check >0)
+	{
+		*line = getN(&memory[fd]);
+		return (1);
+	}
+	if (memory[fd])
+	{
+		free(memory[fd]);
+		memory[fd] = NULL;
+	}
+	return (0);
+}
