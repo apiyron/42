4cfe8c2113ee9a6d1a02f201b2dfbbb644769db2
Andrey Ryabinskiy <apiyron@users.noreply.github.com>
2019-06-22 00:40:41 +0300

    init Fillit

--- 04_Fillit/main.c
+++ 04_Fillit/main.c
@@ -6,7 +6,7 @@
 /*   By: mrhea-ro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/21 23:00:49 by mrhea-ro          #+#    #+#             */
-/*   Updated: 2019/06/21 23:56:59 by mrhea-ro         ###   ########.fr       */
+/*   Updated: 2019/06/22 00:30:36 by mrhea-ro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,27 @@ int main(int argc, char const *argv[])
 {
 	int fd;
 	
+	if (argc != 2)
+	{
+		write(1, "usage: ./fillit target_file\n", 28);
+		return (0);
+	}
 	fd = open(argv[1], O_RDONLY);
-	char c;
-	read(fd, &c, 1);
-	printf("%d", c);
+	char c, c_prev;
+	int line_len = 0;
+	while (read(fd, &c, 1) > 0)
+	{
+		if (line_len > 4)
+		{
+			write(1, "error\n", 6);
+			return (0);
+		}
+		if (c == '\n')
+		{
+			printf("%d\n", line_len);
+			line_len = -1;
+		}
+		line_len++;
+	}
 	return 0;
 }
