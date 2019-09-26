9f5bdd95c684f705b8d4247725f71c0f214d2be2
Andrey Ryabinskiy <apiyron@users.noreply.github.com>
2019-09-26 16:01:20 +0300

    Init finished

--- finished/03_get_next_line/main.c
+++ finished/03_get_next_line/main.c
@@ -6,13 +6,30 @@
 /*   By: mrhea-ro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/26 20:03:15 by mrhea-ro          #+#    #+#             */
-/*   Updated: 2019/05/26 20:03:51 by mrhea-ro         ###   ########.fr       */
+/*   Updated: 2019/09/25 15:34:28 by mrhea-ro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
-void		main()
+int	main(int argc, char **argv)
 {
-	get_next_line(0, 0);
+	int		fd;
+	char	*line;
+
+	if (argc == 1)
+		fd = 0;
+	else if (argc == 2)
+		fd = open(argv[1], O_RDONLY);
+	else
+		return (2);
+	while (get_next_line(fd, &line) == 1)
+	{
+		ft_putendl(line);
+		free(line);
+		getchar();
+	}
+	if (argc == 2)
+		close(fd);
 }
